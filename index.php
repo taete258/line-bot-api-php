c5000aab3cd217d4c6f89f2a1656ee4c51c56b10
Ratchanon Pheungta <44311915+taete258@users.noreply.github.com>
2020-03-11 19:26:34 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,3 @@
 <?php
 echo "Hello LINE BOT \"Ratchanon_59160683!!\"";
 
-
-echo "<img src="https://steamuserimages-a.akamaihd.net/ugc/946220022126942778/F7EA1482B7967952540BB8F0CEE0822048BDE466/" alt="Smiley face" height="300px" width="300px">";
